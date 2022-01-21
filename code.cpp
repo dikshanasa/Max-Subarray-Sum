c6c68fc1f5658595dd162a4142e8fb1293f90af8
Diksha Nasa <64835443+dikshanasa@users.noreply.github.com>
2022-01-21 22:54:32 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+}
+
+int max=INT_MIN;
+for(int i=0;i<n;i++){
+    int sum=0;
+    for(int j=i;j<n;j++){
+        sum=sum+arr[j];
+        if(sum>max){
+            max=sum;
+            
+        }
+        
+    }
+}
+cout<<max;
+}
